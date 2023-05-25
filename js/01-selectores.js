542036e0da3d772ee27b5defe493f8e557fd630e
Ignacio Chavez <nacho_ch3@hotmail.com>
2023-05-25 17:38:54 -0400

    commit de todo

---
+++ js/01-selectores.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // Selector de ID
+    $("#rojo").css("background", "red")
+    .css("color", "white");
+    console.log(rojo);
+
+    $("#amarillo").css("background", "yellow")
+    .css("color", "green");
+
+    $("#verde").css("background", "green")
+    .css("color", "white");
+
+    // Selector de clases
+    let mi_clase = $('.zebra');
+    mi_clase.css("border", "5px dashed black")
+
+    $('.sin_borde')click(function(){
+        console.log("Click dado!!!");
+        $(this).addClass('zebra');
+    });
+});
